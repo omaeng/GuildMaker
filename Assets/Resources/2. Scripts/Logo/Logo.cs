82bdc3de250760fdd8d744cef092e56fa4355f22
omaeng <myeonghuno@gmail.com>
2016-04-22 03:00:58 +0900

    새로운 Script 생성
    Logo화면을 제어하는 스크립트입니다. 로고화면이 일정시간동안 띄워지거나 화면을 터치하면 다음 씬을 불러줍니다.

---
+++ Assets/Resources/2. Scripts/Logo/Logo.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Logo : MonoBehaviour {
+    private GM _gm;
+    private float _time;
+
+    void Awake()
+    {
+        _gm = GameObject.Find("GM").GetComponent<GM>();
+        _time = 0;
+    }
+
+    void Start()
+    {
+        StartCoroutine(PlayLogo());
+    }
+
+    IEnumerator PlayLogo()
+    {
+        while (_time < 2.0f || !Input.GetMouseButtonDown(0))
+        {
+            _time += Time.deltaTime;
+            Debug.Log("PlayLogo!");
+
+            yield return null;
+        }
+
+        _gm.SceneConversion();
+        
+        yield return null;
+    }
+}
