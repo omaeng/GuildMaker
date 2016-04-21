0faf9a8c9118d532801e4ddfcd32a5107a9384fc
omaeng <myeonghuno@gmail.com>
2016-04-22 03:06:18 +0900

    2016. 04. 21 작업
    16. 04. 21 작업까지 작업한 내용입니다.

--- Assets/Resources/2. Scripts/GM.cs
+++ Assets/Resources/2. Scripts/GM.cs
@@ -2,10 +2,47 @@
 using UnityEngine;
 using System.Collections;
 
+using UnityEngine.SceneManagement;
+
 public class GM : MonoBehaviour {
     
+    [SerializeField] private int sceneNum;
+
     void Awake()
     {
+        sceneNum = 0;
         GameObject.DontDestroyOnLoad(this.gameObject);
+    }
+
+    public void SceneConversion(int repeat = 1, bool addScene = false)
+    {
+        for(int count = 0; count < repeat; count++)
+        {
+            IncreseSceneNum();
+        }
+        switch(addScene)
+        {
+            case true:
+                OperateLoadScene(LoadSceneMode.Additive);
+                break;
+            case false:
+                OperateLoadScene();
+                break;
+        }
+    }
+
+    void IncreseSceneNum()
+    {
+        sceneNum++;
+    }
+
+    void OperateLoadScene(LoadSceneMode mode = LoadSceneMode.Single)
+    {
+        SceneManager.LoadScene(sceneNum, mode);
+    }
+
+    void Start()
+    {
+
     }
 }
